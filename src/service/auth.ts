b371f65502e082062d88df5c8df6a9e67fa8df90
Great Egbuna <egbunagreat01@gmail.com>
2025-04-12 18:49:57 +0100

    build bug

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -43,7 +43,7 @@ class AuthService {
           // ..
         });
     } catch (error) {
-      console.log(error?.message);
+      console.log(error);
       return new Error("Internal Server Error");
     }
   }
